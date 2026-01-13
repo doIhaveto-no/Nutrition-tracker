5ea12d9a66d939df8f19dab4019eb8dfed2011ff
ender <enderdev7379@gmail.com>
2026-01-13 19:59:51 +0100

    Fix header

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,12 +4,13 @@ import Header from "./components/Header";
 function Root() {
     return (
         <>
-            {
-                //<Header/>
-            }
+            <Header/>
             <Outlet/>
             <footer style={{background:'#decaca', color:'#292121', padding:'20px 20px',textAlign:'center',marginTop:'auto'}}>
                 <p style={{fontSize:'12px'}}>Nutrition tracker - Pingvini sa madagaskara</p>
+                {
+                    window.location.href + '\n' + '\n' + window.location.pathname
+                }
             </footer>
         </>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,30 @@
 function Header() {
+    function drstr(path) {
+        window.location.pathname = path;
+    }
+
     return (
         <header style={{background:'#fcf5f5', padding: '15px 0', position: 'fixed', width: '100%',top:'0',zIndex:'1000'}}>
             <nav style={{display:'flex', justifyContent: 'space-between', alignItems: 'center', maxWidth: '1200px', margin: '0 auto', padding: '0 20px'}}>
                 {/* videcu da li ce ovo trebati <div style={{display:'flex',gap:'15px',alignItems:'center'}}></div>*/}
-                <button onClick={() =>drstr('pocetna')} 
-                    style={{padding:'10px 20px', background: trenstr ==='pocetna'?'rgba(255,255,255,0.2)' : 'transparent',
+                <button onClick={() =>drstr('home')} 
+                    style={{padding:'10px 20px', background: window.location.pathname.startsWith("home") ? 'rgba(255,255,255,0.2)' : 'transparent',
                         color:'#292121', border:'none'
                     }}>
                     Početna
                 </button>
                 <button onClick={() =>drstr('namirnice')} 
-                    style={{padding:'10px 20px', background: trenstr ==='namirnice'?'rgba(255,255,255,0.2)' : 'transparent',
+                    style={{padding:'10px 20px', background: window.location.pathname.startsWith("namirnice")?'rgba(255,255,255,0.2)' : 'transparent',
                         color:'#292121', border:'none'}}>
                     Namirnice
                 </button>
-                <button onClick={() =>drstr('kontakt')} 
-                    style={{padding:'10px 20px', background: trenstr ==='kontakt'?'rgba(255,255,255,0.2)' : 'transparent',
+                <button onClick={() =>drstr('contact')} 
+                    style={{padding:'10px 20px', background: window.location.pathname.startsWith("contact") ? 'rgba(255,255,255,0.2)' : 'transparent',
                         color:'#292121', border:'none'}}>
                     Kontakt
                 </button>
                 <button onClick={() =>drstr('login')} 
-                    style={{padding:'10px 20px', background: trenstr ==='login'?'rgba(255,255,255,0.2)' : 'transparent',
+                    style={{padding:'10px 20px', background: window.location.pathname.startsWith("login")?'rgba(255,255,255,0.2)' : 'transparent',
                         color:'#292121', border:'none'}}>
                     Prijavi se
                 </button>
