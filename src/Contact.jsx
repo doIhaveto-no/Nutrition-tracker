3b77f2ab3a5dcf6f6fd1bd1de3dfb9c0ccc0a8d4
doIhaveto-no <254255623+doIhaveto-no@users.noreply.github.com>
2026-01-15 22:45:14 +0100

    popravljene stranice kontakt i prijavi se

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,7 @@
 function Contact() {
     return (
         <>            
-            <section className="full-screen" style={{background:'linear-gradient(#decaca,#f58488 0%,#ff1a22 100%)'}}>
+            <section className="glavno" >
                 <h1>How to contact us: dont</h1>
                 {/* <p style={{textAlign:'center'}}>Pronađeno {filterhrane.lenght} namirnica</p>*/}
                 {/* ovde ide lepo da se prikazu pronadjenje namirnice */}
